aa5fe472332020cd3b91b8122215e937b6fb8f17
Alex Tarpley <alex.tarpley@du.edu>
2025-06-25 11:52:46 -0500

    Azure and Git deployment separated

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath: process.env.NODE_ENV === 'production' ? '/CGApp' : '',
   images: {
     unoptimized: true
   }
